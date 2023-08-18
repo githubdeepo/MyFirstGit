b0daa865ab0e74abf42c91189147f39fc1740d27
Deepak kumar <139474117+githubdeepo@users.noreply.github.com>
2023-08-18 15:45:27 +0530

    Add files via upload
    second code

---
+++ Anagram.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+public class Anagram {
+	public static void main(String[] args) {
+		
+  String s1="care";
+  String s2="race";
+  if(s1.length()==s2.length()) {
+           char[] c1=s1.toCharArray();
+           char[] c2=s1.toCharArray();
+           Arrays.sort(c1);
+           Arrays.sort(c2);
+           boolean result=Arrays.equals(c1, c2);
+           if(result==true) {
+        	   System.out.println("Anagram String");
+                }else {
+                	    System.out.println("Not a Anagram String");
+                      }
+	   }else {
+		        System.out.println("Not a Anagram String");
+	         }
+  }
+}
